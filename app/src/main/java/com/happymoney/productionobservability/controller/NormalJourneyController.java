a2b367402f6598da07a54d2a086de20005907213
lvenugopal <lvenugopal@happymoney.com>
2021-11-17 18:19:20 -0500

    Added import statement

--- app/src/main/java/com/happymoney/productionobservability/controller/NormalJourneyController.java
+++ app/src/main/java/com/happymoney/productionobservability/controller/NormalJourneyController.java
@@ -9,6 +9,7 @@ import org.json.simple.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
